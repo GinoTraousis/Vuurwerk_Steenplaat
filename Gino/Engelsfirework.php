f83494b37a9791f49cf7829e62f328a4f63ab155
GinoTraousis <59014950+GinoTraousis@users.noreply.github.com>
2020-01-27 14:08:37 +0100

    Add files via upload

---
+++ Gino/Engelsfirework.php
@@ -0,0 +1,85 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Auteur: Gino Traousis-->
+<title>Ginos Vuurwerkshop</title>
+ 
+
+
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="SteenplaatStyle.css">
+<link rel="icon" href="https://t.jwwb.nl/7lDzxoft8Bm_YOEczSgWTXJ7xzM=/688x0/filters:quality(95)/b.jwwb.nl%2Funsplash%2F51Jgx-F0oLM.jpg" type="image/x-icon" />
+<link rel="shortcut icon" href="https://t.jwwb.nl/7lDzxoft8Bm_YOEczSgWTXJ7xzM=/688x0/filters:quality(95)/b.jwwb.nl%2Funsplash%2F51Jgx-F0oLM.jpg" type="image/x-icon" />
+
+</head>
+
+
+<body>
+  
+<ul>
+  
+  <li><a>Home</a></li>
+   
+  <li class="dropdown">
+    <a class="dropbtn">Categorieën</a>
+    <div class="dropdown-content">
+
+    
+    
+
+    <a href="Steenplaat.php">All</a>
+    <a href="Steenplaat.php?cat=Knalvuurwerk">Knalvuurwerk</a>
+    <a href="Steenplaat.php?cat=Siervuurwerk">Siervuurwerk</a>
+
+
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">Over</a></li>
+  <li>
+    
+    <a href="Vuurwerksubmenu.php">Vuurwerk</a>
+
+  </li>
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+ <li class="home"  role="presentation"><a role="menuitem" tabindex="-1" href="Engelsfirework.php">Firework</a></li>
+ </ul>
+   
+
+   
+ </ul>
+
+
+
+
+
+</div>
+
+
+
+ 
+<p>          <h1>Yes/no firework?</h1>
+
+<h3>Prohibition</h3>
+Yes or no firework? That is the question of many people in this country. Each year you hear there is trouble with firework, Only is it in the recent years a lot more: firework that is throwing to policecars, More and more firework are causing destruction but the biggest trouble is that its causing nuisance. 
+<br> 
+<br>
+<h3>Environment</h3>
+There is also been problems with nitrogen and fireworks only making it even worse then. By throwing firework comes chemical fabrics and heavy metal in the air, bottom and groundwater. On new years eve is it in the city 30 times worse.
+<br>
+<br>
+<h3>Safety</h3>
+Nearly 500 people ended up injured every year during the newyear, an average of one death per year and around 11.000 incidents are registered. Its in the Netherland the most unsafe party of the year.  
+<br>
+<br>
+<h3>My opinion</h3>
+My opinion is that firework should be forbidden. Its causing nuisance for people and the animals. For the animal its the worst thing because they dont know what is happening . Safety is of course one of the reasons why I think it should be forbidden. And the nitrogen is already a big problem with cars, firework is only making it worse more and more.
+</p>
+
+
+
+</body>
+
+
+
+</html>
---
+++ Gino/Vuurwerksubmenu.php
@@ -0,0 +1,85 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Auteur: Gino Traousis-->
+<title>Ginos Vuurwerkshop</title>
+ 
+
+
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="SteenplaatStyle.css">
+<link rel="icon" href="https://t.jwwb.nl/7lDzxoft8Bm_YOEczSgWTXJ7xzM=/688x0/filters:quality(95)/b.jwwb.nl%2Funsplash%2F51Jgx-F0oLM.jpg" type="image/x-icon" />
+<link rel="shortcut icon" href="https://t.jwwb.nl/7lDzxoft8Bm_YOEczSgWTXJ7xzM=/688x0/filters:quality(95)/b.jwwb.nl%2Funsplash%2F51Jgx-F0oLM.jpg" type="image/x-icon" />
+
+</head>
+
+
+<body>
+  
+<ul>
+  
+  <li><a>Home</a></li>
+   
+  <li class="dropdown">
+    <a class="dropbtn">Categorieën</a>
+    <div class="dropdown-content">
+
+    
+    
+
+    <a href="Steenplaat.php">All</a>
+    <a href="Steenplaat.php?cat=Knalvuurwerk">Knalvuurwerk</a>
+    <a href="Steenplaat.php?cat=Siervuurwerk">Siervuurwerk</a>
+
+
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">Over</a></li>
+  <li>
+    
+    <a class="home"href="Vuurwerksubmenu.php">Vuurwerk</a>
+
+  </li>
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+ <li  role="presentation"><a role="menuitem" tabindex="-1" href="Engelsfirework.php">Firework</a></li>
+ </ul>
+   
+
+   
+ </ul>
+
+
+
+
+
+</div>
+
+
+
+ 
+<p>          <h1>Wel/Geen vuurwerk?</h1>
+
+<h3>Verbod</h3>
+Wel of geen vuurwerk? Dat is de vraag van vele mensen in het land. Elk jaar hoor je wel problemen dat ontstaat door vuurwerk, alleen zijn het de laatste jaren er steeds meer zoals: vuurwerk dat naar politiebusjes worden gegooid, steeds meer illegaal vuurwerk wat voor vernielingen bezorgd en het grootste probleem is de herrie die vuurwerk veroorzaakt. 
+<br> 
+<br>
+<h3>Milieu</h3>
+Ook is er de laatste tijd een probleem met stikstof en vuurwerk verergert dat de problemen. Door het afsteken van vuurwerk komen er chemische stoffen en zware metalen in lucht, bodem en grondwater terecht. Op oud en nieuw is het in de steden de lucht 30 keer zo slecht.
+<br>
+<br>
+<h3>Veiligheid </h3>
+Tijdens de jaarwisseling eindigen jaarlijks bijna 500 mensen gewond, valt gemiddeld één dode per jaar en worden zo’n 11.000 incidenten geregistreerd. Het is in Nederland de onveiligste feest van het jaar. 
+<br>
+<br>
+<h3>Mijn mening</h3>
+Mijn mening is dat vuurwerk verboden moet worden. Omdat het voor veel overlast zorgen voor de mens en voor de dieren. Voor de dieren is het allerergst omdat zij niet weten wat er gebeurd en dan heel erg schrikken. Veiligheid is natuurlijk ook een van de reden waarom ik vindt dat het verboden moet worden zoals hier boven staat want er jaarlijks tienduizend incidenten. En de stikstof is al een groot probleem met de auto’s en vuurwerk verergert het alleen maar meer.
+</p>
+
+
+
+</body>
+
+
+
+</html>
